1599f53a3b31627359ba62a95b4c61b40e84a541
jj-tena <josejustotena0@gmail.com>
2024-04-28 21:41:47 +0200

    Create basic navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
-import './App.css';
-import axios from 'axios';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './views/Home';
+import Login from './views/Login';
+import Register from './views/Register';
+import AddAnalytic from './views/AddAnalytic';
+import Analytics from './views/Analytics';
+import Evolution from './views/Evolution';
+
 
 function App() {
-
-  axios.get('http://localhost:8000/api/users/')
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-  });
   
   return (
-    <h1>
-        Hello world
-    </h1>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/add-analytic" component={AddAnalytic} />
+        <Route path="/analytics" component={Analytics} />
+        <Route path="/evolution" component={Evolution} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ frontend/src/views/AddAnalytic/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <h1>Add analytic</h1>
+    )
+}
+
+export default Home;
