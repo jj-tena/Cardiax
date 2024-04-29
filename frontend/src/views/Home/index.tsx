b81213a0c4225152bd76bf6459aafb329342e378
jj-tena <josejustotena0@gmail.com>
2024-04-29 14:08:27 +0200

    Import material ui

--- frontend/src/views/Home/index.tsx
+++ frontend/src/views/Home/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import HomeStyles from "./Home.styles";
 import useHome from "./Home";
+import { Container, Typography } from "@mui/material";
 
 const Home = () => {
 
     const {} = useHome();
 
     return (
-        <div style={HomeStyles.main}>
-            <h1>Home</h1>
-        </div>
+        <Container style={HomeStyles.main}>
+            <Typography>Home</Typography>
+        </Container>
     )
 }
 
